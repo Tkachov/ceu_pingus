41cf7456c68e1617b7b79455630d0e48f6f8c9a8
Tkachov <Tkachov@TLab>
2015-06-10 00:39:42 +0600

    BlockerAction

--- src/pingus/actions/blocker.hpp
+++ src/pingus/actions/blocker.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -33,11 +33,12 @@ public:
   ActionName::Enum get_type() const { return ActionName::BLOCKER; }
 
   void  draw (SceneContext& gc);
-  void  update();
+  void update() {};
 
 private:
   bool  standing_on_ground();
   void  catch_pingu(Pingu* pingu);
+  void catch_pingus();
 
   Blocker (const Blocker&);
   Blocker& operator= (const Blocker&);
