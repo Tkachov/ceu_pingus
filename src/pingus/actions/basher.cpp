56b9f79a617ade69dd51021064417d448c85744f
Tkachov <Tkachov@TLab>
2015-06-09 16:14:42 +0600

    BasherAction

--- src/pingus/actions/basher.cpp
+++ src/pingus/actions/basher.cpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -37,9 +37,9 @@ Basher::Basher (Pingu* p) :
 {
   assert(bash_radius.get_width() % 2 == 0);
 
-  sprite.load(Direction::LEFT,  Sprite("pingus/player" + 
+  sprite.load(Direction::LEFT,  Sprite("pingus/player" +
                                        pingu->get_owner_str() + "/basher/left"));
-  sprite.load(Direction::RIGHT, Sprite("pingus/player" + 
+  sprite.load(Direction::RIGHT, Sprite("pingus/player" +
                                        pingu->get_owner_str() + "/basher/right"));
 
   bash_reach = bash_radius.get_width();
@@ -55,56 +55,7 @@ Basher::draw (SceneContext& gc)
   gc.color().draw(sprite[pingu->direction], pingu->get_pos());
 }
 
-void
-Basher::update ()
-{
-  sprite[pingu->direction].update();
-
-  ++basher_c;
-  if (basher_c % 3 == 0)
-  {
-    if (!walk_forward())
-    {
-      // FIXME: set_action() in walk_forward() makes the Basher object
-      // invalid, thus making all further access illegal and thus
-      // forces the return here
-      return;
-    }
-
-    // If on walking forward the Basher has now walked on to water or lava
-    if (rel_getpixel(0, -1) == Groundtype::GP_WATER
-        || rel_getpixel(0, -1) == Groundtype::GP_LAVA)
-    {
-      pingu->set_action(ActionName::DROWN);
-    }
-    // If walking on to something (i.e. hasn't fallen)
-    else if (rel_getpixel(0, -1) != Groundtype::GP_NOTHING)
-    {
-      // If the Basher has walked into something that it won't be able to
-      // bash
-      if (rel_getpixel(0, 0) == Groundtype::GP_SOLID
-          || rel_getpixel(0, pingu_height) == Groundtype::GP_SOLID)
-      {
-        // Change direction and let walk code walk forward/up to get out.
-        Sound::PingusSound::play_sound("chink");
-        pingu->direction.change();
-        pingu->set_action(ActionName::WALKER);
-      }
-      else if (have_something_to_dig())
-      {
-        // We only bash every second step, cause the Pingus would
-        // get trapped otherwise in the bashing area.
-        if (basher_c % 2 == 0)
-          bash();
-      }
-      else if (static_cast<float>(sprite[pingu->direction].get_current_frame()) // FIXME: Game logic must be separate from Sprite 
-               / static_cast<float>(sprite[pingu->direction].get_frame_count()) > 0.6f)
-      { // FIXME: EVIL! Engine must not relay on graphic
-        pingu->set_action(ActionName::WALKER);
-      }
-    }
-  }
-}
+void Basher::update() {}
 
 void
 Basher::bash()
--- src/pingus/actions/basher.hpp
+++ src/pingus/actions/basher.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -27,8 +27,7 @@ class Basher : public PinguAction
 {
 private:
   StateSprite   sprite;
-  CollisionMask bash_radius;
-  int  basher_c;
+  CollisionMask bash_radius;  
   bool first_bash;
 
   /** The no. of pixels ahead that a Basher checks for something bashable.
