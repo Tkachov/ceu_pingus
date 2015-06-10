208935ad1fc2b160f401252b00835eb0fe7e70b0
Tkachov <Tkachov@TLab>
2015-06-10 10:37:40 +0600

    WaiterAction

--- src/pingus/actions/waiter.cpp
+++ src/pingus/actions/waiter.cpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,24 +23,9 @@ namespace Actions {
 
 Waiter::Waiter (Pingu* p) :
   PinguAction(p),
-  countdown(2.0f),
   sprite()
 {
   sprite = Sprite("pingus/player" + pingu->get_owner_str() + "/waiter/left");
-}
-
-void
-Waiter::update ()
-{
-  sprite.update();
-
-  if (countdown < 0)
-  {
-    pingu->set_action(ActionName::WALKER);
-    return;
-  }
-
-  countdown -= 0.025f;
 }
 
 void
