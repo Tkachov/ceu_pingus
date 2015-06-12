66b2720463b8acfe37516d87cd239bc55b352d5e
Tkachov <Tkachov@TLab>
2015-06-12 15:04:46 +0600

    SupermanAction
    Another unnecessary "new" action, used in "test/test1" only.

--- src/pingus/actions/superman.hpp
+++ src/pingus/actions/superman.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,24 +22,19 @@
 
 namespace Actions {
 
-class Superman : public PinguAction
-{
+class Superman: public PinguAction {
 private:
-  float counter;
-  float x_pos;
   Sprite sprite;
 
 public:
-  Superman (Pingu*);
-
+  Superman(Pingu* p): PinguAction(p), sprite() {};
   ActionName::Enum get_type () const { return ActionName::SUPERMAN; }
-
-  void draw (SceneContext& gc);
-  void update ();
+  void draw(SceneContext& gc) {};
+  void update() {};
 
 private:
-  Superman (const Superman&);
-  Superman& operator= (const Superman&);
+  Superman(const Superman&);
+  Superman& operator=(const Superman&);
 };
 
 } // namespace Actions
