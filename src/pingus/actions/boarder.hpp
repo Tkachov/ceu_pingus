f8e89cc75283f44778bba872569ccae6c13d0dfe
Tkachov <Tkachov@TLab>
2015-06-12 14:49:55 +0600

    BoarderAction
    Ported `draw()` & `update()` and fixed sprite.
    
    Again a "new" action which is used in "test/test1" only.

--- src/pingus/actions/boarder.hpp
+++ src/pingus/actions/boarder.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -24,25 +24,18 @@ namespace Actions {
 
 /** The Boarder action causes a pingu to use a skateboard to move
     forward. */
-class Boarder : public PinguAction
-{
+class Boarder: public PinguAction {
 private:
-  float x_pos;
-  float speed;
   StateSprite sprite;
 public:
-  Boarder (Pingu* p);
-
-  ActionName::Enum get_type () const { return ActionName::BOARDER; }
-
-  void  draw (SceneContext& gc);
-  void  update ();
+  Boarder(Pingu* p): PinguAction(p), sprite() {};
+  ActionName::Enum get_type() const { return ActionName::BOARDER; }
+  void draw(SceneContext& gc) {};
+  void update() {};
 
 private:
-  bool on_ground ();
-
-  Boarder (const Boarder&);
-  Boarder& operator= (const Boarder&);
+  Boarder(const Boarder&);
+  Boarder& operator=(const Boarder&);
 };
 
 } // namespace Actions
