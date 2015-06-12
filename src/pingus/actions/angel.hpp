53d0af7cc79e6b79d31b8b526c160c23fa8c27a2
Tkachov <Tkachov@TLab>
2015-06-12 13:38:16 +0600

    AngelAction
    * update();
    * draw();
    * fixed its speed to be smooth enough;
    * added a texture for the level it's used on ("test/test1").
    
    //It's used ONLY on that level, there is no purpose of that action, and
    even a sprite is too transparent.

--- src/pingus/actions/angel.hpp
+++ src/pingus/actions/angel.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,24 +22,19 @@
 
 namespace Actions {
 
-class Angel : public PinguAction
-{
+class Angel: public PinguAction {
 private:
-  float counter;
-  float x_pos;
   Sprite sprite;
 
 public:
-  Angel (Pingu* p);
-
-  ActionName::Enum get_type () const { return ActionName::ANGEL; }
-
-  void  update ();
-  void  draw (SceneContext& gc);
+  Angel(Pingu* p): PinguAction(p), sprite() {};
+  ActionName::Enum get_type() const { return ActionName::ANGEL; }
+  void update() {};
+  void draw(SceneContext& gc) {};
 
 private:
-  Angel (const Angel&);
-  Angel& operator= (const Angel&);
+  Angel(const Angel&);
+  Angel& operator=(const Angel&);
 };
 
 } // namespace Actions
