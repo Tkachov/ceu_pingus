65969dd5aff50fef823cea6f7786d649d7647a53
Tkachov <Tkachov@TLab>
2015-06-12 12:36:53 +0600

    BomberAction
    Plus:
    * pingu.ceu fix;
    * AWAIT_N and PRE_CONDITION_AWAIT macros added, used in BridgerAction
    and BomberAction.

--- src/pingus/actions/bomber.cpp
+++ src/pingus/actions/bomber.cpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -25,33 +25,13 @@
 
 namespace Actions {
 
-Bomber::Bomber (Pingu* p) :
-  PinguAction(p),  
-  gfx_exploded(false),  
+Bomber::Bomber(Pingu* p):
+  PinguAction(p),
   bomber_radius("other/bomber_radius_gfx", "other/bomber_radius"),
   sprite(),
   explo_surf(Sprite("pingus/player" + pingu->get_owner_str() + "/explo")),
   mover(WorldObj::get_world(), Vector3f(0, 0, 0))
-{
-  sprite.load(Direction::LEFT,  "pingus/player" + pingu->get_owner_str() + "/bomber/left");
-  sprite.load(Direction::RIGHT, "pingus/player" + pingu->get_owner_str() + "/bomber/right");
-
-  WorldObj::get_world()->play_sound("ohno", pingu->get_pos ());
-}
-
-void
-Bomber::draw (SceneContext& gc)
-{
-  if (sprite[pingu->direction].get_current_frame() >= 13 && !gfx_exploded)
-  {
-    gc.color().draw (explo_surf, Vector3f(pingu->get_x () - 32, pingu->get_y () - 48));
-    gfx_exploded = true;
-  }
-
-  gc.color().draw(sprite[pingu->direction], pingu->get_pos ());
-}
-
-void Bomber::update() {}
+{}
 
 } // namespace Actions
 
--- src/pingus/actions/bomber.hpp
+++ src/pingus/actions/bomber.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -26,26 +26,23 @@ namespace Actions {
 
 /** An action with lets the Pingu explode. After the explosion the the
     Pingu leaves a hole inside the ground. */
-class Bomber : public PinguAction
-{
-private:  
-  bool gfx_exploded;  
-
+class Bomber: public PinguAction {
+private:
   CollisionMask bomber_radius;
   StateSprite sprite;
 
-  Sprite   explo_surf;
+  Sprite explo_surf;
   Movers::LinearMover mover;
 
 public:
-  Bomber (Pingu* p);
+  Bomber(Pingu*);
 
   ActionName::Enum get_type() const { return ActionName::BOMBER; }
 
   bool change_allowed (ActionName::Enum action) { return false; }
 
-  void draw (SceneContext& gc);
-  void update();
+  void draw(SceneContext& gc) {};
+  void update() {};
 
 private:
   Bomber (const Bomber&);
