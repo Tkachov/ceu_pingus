31af325e3f195b6a3ebb2c764a0a9ae05c465354
Tkachov <Tkachov@TLab>
2015-06-24 18:46:05 +0600

    LaserExit

--- src/pingus/worldobjs/laser_exit.cpp
+++ src/pingus/worldobjs/laser_exit.cpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,62 +21,29 @@
 #include "pingus/pingu_holder.hpp"
 #include "pingus/world.hpp"
 
+#include "ceuvars.h"
+
 namespace WorldObjs {
 
 LaserExit::LaserExit(const FileReader& reader) :
   surface("traps/laser_exit"),
-  pos(),
-  killing(false)
+  pos()
 {
   reader.read_vector("position", pos);
+
+  LaserExit* self = this;
+  ceu_out_go(&CEUapp, CEU_IN_NEW_LASER_EXIT, &self);
+}
+
+LaserExit::~LaserExit() {
+  LaserExit* self = this;
+  ceu_out_go(&CEUapp, CEU_IN_DELETE_LASER_EXIT, &self);
 }
 
 float
 LaserExit::get_z_pos () const
 {
   return pos.z;
-}
-
-void
-LaserExit::draw (SceneContext& gc)
-{
-  gc.color().draw (surface, pos);
-}
-
-void
-LaserExit::update ()
-{
-
-  PinguHolder* holder = world->get_pingus();
-  for (PinguIter pingu = holder->begin (); pingu != holder->end (); ++pingu){
-    catch_pingu(*pingu);
-  }
-
-  if (killing) {
-    if (surface.is_finished()) {
-      surface.restart();
-      killing = false;
-    } else {
-      surface.update();
-    }
-  }
-}
-
-void
-LaserExit::catch_pingu (Pingu* pingu)
-{
-  if (!killing)
-  {
-    if (   pingu->get_x () < pos.x + 34 + 10 && pingu->get_x () > pos.x + 34
-           && pingu->get_y () < pos.y + 43 + 20 && pingu->get_y () > pos.y + 43)
-    {
-      if (pingu->get_action() != ActionName::LASERKILL)
-      {
-        killing = true;
-        pingu->set_action(ActionName::LASERKILL);
-      }
-    }
-  }
 }
 
 } // namespace WorldObjs
--- src/pingus/worldobjs/laser_exit.hpp
+++ src/pingus/worldobjs/laser_exit.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -35,20 +35,13 @@ private:
   Sprite   surface;
   Vector3f pos;
 
-  bool killing;
-
 public:
   LaserExit(const FileReader& reader);
+  ~LaserExit();
 
   float get_z_pos () const;
   void set_pos(const Vector3f& p) { pos = p; }
   Vector3f get_pos() const { return pos; }
-
-  void draw (SceneContext& gc);
-  void update ();
-
-protected:
-  void catch_pingu (Pingu*);
 
 private:
   LaserExit (const LaserExit&);
