ea74bd0e81ab2f73a78d0c2d38524c776952a6d6
Tkachov <Tkachov@TLab>
2015-06-24 19:20:11 +0600

    Smasher
    Now we can remove `set_action` from C++ Pingu

--- src/pingus/worldobjs/smasher.hpp
+++ src/pingus/worldobjs/smasher.hpp
@@ -5,12 +5,12 @@
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -34,23 +34,15 @@ private:
   Sprite  sprite;
   Vector3f pos;
 
-  bool smashing;
-  bool downwards;
-  int  count;
-
 public:
   Smasher(const FileReader& reader);
+  ~Smasher();
 
   float get_z_pos () const;
   void set_pos(const Vector3f& p) { pos = p; }
   Vector3f get_pos() const { return pos; }
 
-  void draw (SceneContext& gc);
   void on_startup ();
-  void update ();
-
-protected:
-  void catch_pingu (Pingu* pingu);
 
 private:
   Smasher (const Smasher&);
